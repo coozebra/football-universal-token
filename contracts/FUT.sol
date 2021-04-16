19f24322de4fc1b4947b5461cfb7d6c682f722ab
coozebra <timnbil@hotmail.com>
2021-04-16 18:29:53 -0500

    add diamond

--- contracts/FUT.sol
+++ contracts/FUT.sol
@@ -1,25 +1,13 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-import "@solidstate/contracts/access/OwnableStorage.sol";
 import "@solidstate/contracts/access/SafeOwnable.sol";
 import "@solidstate/contracts/token/ERC20/ERC20.sol";
-import "@solidstate/contracts/token/ERC20/ERC20MetadataStorage.sol";
-import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract FUT is ERC20, SafeOwnable {
-    /**
-     * Contract initialization.
-     *
-     * The `constructor` is executed only once when the contract is created.
-     */
-    constructor() {
-        ERC20MetadataStorage.Layout storage l = ERC20MetadataStorage.layout();
-        l.name = "FUT - Football Universal Token";
-        l.symbol = "FUT";
-
-        _mint(msg.sender, 1000000 ether);
-
-        OwnableStorage.layout().owner = msg.sender;
-    }
+  function mint (
+    uint amount
+  ) external onlyOwner {
+    _mint(msg.sender, amount);
+  }
 }
---
+++ contracts/FUTDiamond.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "@solidstate/contracts/proxy/diamond/Diamond.sol";
+import "@solidstate/contracts/access/OwnableStorage.sol";
+import "@solidstate/contracts/token/ERC20/ERC20MetadataStorage.sol";
+
+contract FUTDiamond is Diamond {
+  constructor() {
+    ERC20MetadataStorage.Layout storage l = ERC20MetadataStorage.layout();
+    l.name = "FUT - Football Universal Token";
+    l.symbol = "FUT";
+
+    OwnableStorage.layout().owner = msg.sender;
+  }
+}
