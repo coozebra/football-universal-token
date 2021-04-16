25a9e3812e81df9fe77079b0f108fcd92d61c61f
coozebra <timnbil@hotmail.com>
2021-04-16 16:54:11 -0300

    agrego files

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`]
+    },
+    // mainnet: {
+    //   url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+    //   accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`]
+    // }
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.MAINNET_PRIVATE_KEY}`],
+      gasPrice: Number(160.1 * 1000000000)
+    }
+  }
+};
