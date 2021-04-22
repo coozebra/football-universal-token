c548726217e19a136378c62d22e416a6895e1e63
coozebra <timnbil@hotmail.com>
2021-04-22 10:16:13 -0300

    addig Readme Milestones

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # FUT
+Is the Football Universal Token. We dream to implement it as the main Token in the Football Ecosystem. 
+
+## Technical Milestones:
+- [x] Get Token in ETH
+- [ ] Make the Standard Contract Token with Basic Functions
+- [ ] Make it Compilable
+- [ ] Make it Versionable (hardhat-upgrade || diamond)
+- [ ] Tested in Localhost
+- [ ] Tested in Ropsten
+- [ ] Tested in Kovan
+- [ ] Upgrade Version in Mainnet.
+- [ ] Make First transacction trhough Mainnet.
+- [ ] Add FUT into a Liquidity Pool trhough Uniswap.
+- [ ] Make the FUT Wallet.
+- [ ] Do transactions between Wallets
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,16 +3,29 @@ async function main() {
     const [deployer] = await ethers.getSigners();
 
     console.log(
-        "Deploying contracts with the account:",
+        "Deploying contracts with the account:      ",
         deployer.address
     );
 
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log("Account balance:                           ", (await deployer.getBalance()).toString());
 
     const Token = await ethers.getContractFactory("FUT");
     const token = await Token.deploy();
 
-    console.log("Token address:", token.address);
+    console.log("Token address:                             ", token.address);
+
+    const facetCuts = [
+        {
+          target: token.address,
+          action: 0,
+          selectors: Object.keys(token.interface.functions).map(fn => token.interface.getSighash(fn)),
+        },
+      ];
+    
+    const FUTDiamond = await ethers.getContractFactory('FUTDiamond');
+    const futDiamond = await FUTDiamond.deploy();
+    await futDiamond.deployed();
+    await FUTDiamond.connect(deployer).diamondCut(facetCuts, ethers.constants.AddressZero, '0x');
 
 }
   
